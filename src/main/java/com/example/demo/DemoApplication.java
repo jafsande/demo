62451fb3d0627bfba0461eb010eaa4da4972e18c
unknown <0006610@viewnext.com>
2021-04-15 19:21:57 +0200

    Añadido archivo de prueba en rama test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Process exec;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String cmdArg = args[0];
+		try {
+			Runtime.getRuntime().exec(cmdArg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
